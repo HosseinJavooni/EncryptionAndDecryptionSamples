fe7efb6e9956f41642322c24f3d15d1c21a1043e
HosseinJavooni <h.javooni@faraboom.co>
2023-05-30 19:01:45 +0330

    1402/03/09_1901pm

---
+++ src/main/java/RSA/DoRSAEncryptAndDecryptWithKeyFileAndString.java
@@ -0,0 +1,70 @@
+package RSA;
+
+import org.apache.commons.codec.binary.Base64;
+
+import javax.crypto.Cipher;
+import java.io.File;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.security.KeyFactory;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+
+public class DoRSAEncryptAndDecryptWithKeyFileAndString {
+
+        // Decrypt using RSA public key
+        public String decryptMessage(String encryptedText, PublicKey publicKey) throws Exception {
+            Cipher cipher = Cipher.getInstance("RSA");
+            cipher.init(Cipher.DECRYPT_MODE, publicKey);
+            return new String(cipher.doFinal(Base64.decodeBase64(encryptedText)));
+        }
+
+        // Encrypt using RSA private key
+        public String encryptMessage(String plainText, PrivateKey privateKey) throws Exception {
+            Cipher cipher = Cipher.getInstance("RSA");
+            cipher.init(Cipher.ENCRYPT_MODE, privateKey);
+            return Base64.encodeBase64String(cipher.doFinal(plainText.getBytes()));
+        }
+
+        public PrivateKey readPrivateKeyFromFile() throws Exception {
+            File privateKeyFile = new File("src/main/resources/privateKey.anyExtention");
+            String key = new String(Files.readAllBytes(privateKeyFile.toPath()), Charset.defaultCharset());
+
+            // We can give private key as a String here also
+            String privateKeyPEM = key
+                    .replace("-----BEGIN PRIVATE KEY-----", "")
+                    .replaceAll(System.lineSeparator(), "")
+                    .replace("-----END PRIVATE KEY-----", "");
+
+            System.out.println("=====================================================");
+            System.out.println("Private key is: \n" + privateKeyPEM);
+            System.out.println("=====================================================");
+
+            byte[] encoded = Base64.decodeBase64(privateKeyPEM);
+
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(encoded);
+            return keyFactory.generatePrivate(keySpec);
+        }
+
+        public PublicKey readPublicKeyFromFile() throws Exception {
+            File privateKeyFile = new File("src/main/resources/publicKey.anyExtention");
+            String key = new String(Files.readAllBytes(privateKeyFile.toPath()), Charset.defaultCharset());
+
+            String publicKeyPEM = key
+                    .replace("-----BEGIN PUBLIC KEY-----", "")
+                    .replaceAll(System.lineSeparator(), "")
+                    .replace("-----END PUBLIC KEY-----", "");
+            System.out.println("=====================================================");
+            System.out.println("Public key is: \n" + publicKeyPEM);
+            System.out.println("=====================================================");
+
+            byte[] encoded = Base64.decodeBase64(publicKeyPEM);
+
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            X509EncodedKeySpec keySpec = new X509EncodedKeySpec(encoded);
+            return keyFactory.generatePublic(keySpec);
+        }
+    }
---
+++ src/main/java/RSA/RSAMain.java
@@ -0,0 +1,19 @@
+package RSA;
+
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
+public class RSAMain {
+    public static void main(String[] args) throws Exception {
+        String plainText = "Hello RSA Encryption!";
+        DoRSAEncryptAndDecryptWithKeyFileAndString rsaEncryptionJava = new DoRSAEncryptAndDecryptWithKeyFileAndString();
+        PrivateKey privateKey = rsaEncryptionJava.readPrivateKeyFromFile();
+        PublicKey publicKey = rsaEncryptionJava.readPublicKeyFromFile();
+        String encryptedText = rsaEncryptionJava.encryptMessage(plainText, privateKey);
+        String descryptedText = rsaEncryptionJava.decryptMessage(encryptedText, publicKey);
+
+        System.out.println("input:" + plainText);
+        System.out.println("encrypted:" + encryptedText);
+        System.out.println("decrypted:" + descryptedText);
+    }
+}
